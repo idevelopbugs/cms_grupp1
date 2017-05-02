129b49b024dba452b31431d1e0ad9032477c1a44
idevelopbugs <tasteofchains@gmail.com>
2017-05-02 13:13:48 +0200

    First commit

---
+++ includes/db.php
@@ -0,0 +1,11 @@
+<?php
+$options = [ 
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  PDO::ATTR_EMULATE_PREPARES   => false
+];
+
+$pdo = new PDO(
+  "mysql:host=localhost;dbname=cms;charset=utf8",
+  "root",
+  "root", $options);
---
+++ includes/loginform.php
@@ -0,0 +1,5 @@
+          <form method="post" action="login.php" id="loginform" name="loginform">
+Username: 	<input type="text" id="username" maxlength="20" name="username">
+Password: 	<input type="password" id="password" maxlength="20" name="password">	
+          	<input type="submit" name="login" value="Login">
+          </form>
