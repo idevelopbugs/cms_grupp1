a3005707fd9f000a9e21069614bf3eacbd05455e
Janaina Marana <jmarana@Janainas-MacBook-Pro.local>
2017-05-10 15:46:25 +0200

    fixed editor.php

--- includes/editor.php
+++ includes/editor.php
@@ -12,7 +12,7 @@
     echo '<div class="container"
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-sm-12">
-                    <form method="post" action="editpost.php?id="' . $post['ID'] . ' id="postform">
+                    <form method="post" action="editpost.php?id=' . $post['ID'] . '" id="postform">
                         <input class="form-control titlerow" type="text" value="' . $post['Title'] . '" name="title" required>
                         <textarea class="form-control" rows="10" cols="30" name="postcontent" required>' . $post['Content'] . '</textarea>
                         <button class="btn btn-primary addpostbtn" type="submit" name="submit" value="Edit">Edit post</button>
--- includes/editpost.php
+++ includes/editpost.php
@@ -1,5 +1,4 @@
 <?php
     include(dirname(__DIR__) . '/includes/db.php');
-    include "headerloggedin.php";
     $post->editPost();
     header('refresh:2; url=../index.php');
