23847532f665fa20bbe9b1f8161cbf6ff1ccb53e
Liam Öberg <obergliam@gmail.com>
2017-05-24 09:14:05 +0200

    minor fixes

--- includes/postform.php
+++ includes/postform.php
@@ -1,13 +1,13 @@
 <?php
     include(dirname(__DIR__) . '/includes/db.php');
-	include(dirname(__DIR__) . '/includes/header.php');
+	  include(dirname(__DIR__) . '/includes/header.php');
     include(dirname(__DIR__) . '/includes/headerloggedin.php');
 ?>
 <div class="container">
   <div class="row">
     <div class="col-lg-8 col-lg-offset-2 col-sm-12">
       <form method="post" action="createpost.php" id="postform">
-		    <input type="text" class="form-control titlerow" placeholder="Title" name="title" autocomplete="off">
+		    <input type="text" class="form-control titlerow" placeholder="Title" name="title" autocomplete="off" required>
       	<textarea class="form-control" rows="10" cols="30" name="postcontent" required></textarea>
 	  	  <button class="btn btn-primary addpostbtn" type="submit" name="submit" value="Add post">Add post</button>
       </form>
